8bbfb2539cee8a39a5d3ed6182a047b8989fc5e0
Alberto Benatti <albi.benni8@gmail.com>
2023-06-02 17:26:50 +0200

    added deps and loading.tsx

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,14 @@
+import React from 'react';
+
 export default function Loading() {
-  return <div>Loading...</div>;
+  return (
+    <div className="d-block m-auto">
+      <strong>Loading...</strong>
+      <div
+        className="spinner-border m-auto"
+        role="status"
+        aria-hidden="true"
+      ></div>
+    </div>
+  );
 }
