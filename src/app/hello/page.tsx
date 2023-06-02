401c51407276255feadead4acdfcb7837e35fd68
Alberto Benatti <albi.benni8@gmail.com>
2023-06-02 13:49:41 +0200

    loading global file and example in page

--- src/app/hello/page.tsx
+++ src/app/hello/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
+import { resolve } from "path";
+
+export default async function Page() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return <div>Hello page router test</div>;
 }
