5f68f0a31402618395ad499d2cb358b0710cca7a
Alberto Benatti <albi.benni8@gmail.com>
2023-06-10 10:52:11 +0200

    metadate title override

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,7 +1,11 @@
 import { UnplashImage } from '@/models/unplash-image';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Alert } from '@/components/bootstrap';
 
+export const metadata = {
+  title: 'Static fetching - Next.js sandbox 13.4',
+};
 export default async function page() {
   const response = await fetch(
     'https://api.unsplash.com/photos/random?client_id=' +
@@ -13,6 +17,11 @@ export default async function page() {
 
   return (
     <div className="d-flex flex-column align-items-center">
+      <Alert>
+        This page <strong>fetches and caches data at build time</strong>. Even
+        though the Unsplash API always returns a new image, we see the same
+        image after refreshing the page until we compile the project again.
+      </Alert>
       <Image
         src={image.urls.raw}
         width={myWidth}
