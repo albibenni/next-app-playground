fc0ca1ffd095aeb146c32b593e61b42bc945c228
Alberto Benatti <albi.benni8@gmail.com>
2023-06-10 10:53:56 +0200

    static rendering done

--- src/app/hello/page.tsx
+++
@@ -1,6 +0,0 @@
-import { resolve } from "path";
-
-export default async function Page() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  return <div>Hello page router test</div>;
-}
