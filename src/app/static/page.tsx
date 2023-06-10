cee777633f69ca696f13e868c6821af7c72f0e4f
Alberto Benatti <albi.benni8@gmail.com>
2023-06-10 08:09:32 +0200

    static image working with resize url added inside config next

--- src/app/static/page.tsx
+++ src/app/static/page.tsx
@@ -1,4 +1,5 @@
 import { UnplashImage } from '@/models/unplash-image';
+import Image from 'next/image';
 
 export default async function page() {
   const response = await fetch(
@@ -6,4 +7,17 @@ export default async function page() {
       process.env.UNSPLASH_ACCESS_KEY
   );
   const image: UnplashImage = await response.json();
+  const myWidth = Math.min(500, image.width);
+  const myHeight = (myWidth / image.width) * image.height;
+
+  return (
+    <div className="d-flex flex-column align-items-center">
+      <Image
+        src={image.urls.raw}
+        width={myWidth}
+        height={myHeight}
+        alt={image.description}
+      />
+    </div>
+  );
 }
